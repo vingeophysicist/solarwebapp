e60ed0662f94863909d342c8b2fe56213d915b66
vingeophysicist <uobodomavin@gmail.com>
2018-12-08 16:20:05 +0100

    initial commit

--- solar/solar/urls.py
+++ solar/solar/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^general/', include('general.urls'))
+    url(r'^', include('general.urls'))
 ]
